73563c642c09b0d36e7728379a34a6d3b8eaa8b0
Elhafed elyazidi <elhafed.elyazidi@gmail.com>
2020-07-14 18:21:52 +0100

    Fetch Post Comment

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -26,7 +26,7 @@ class CommentForm extends Component {
         }
         HandleSubmit=(values)=>{
             this.toggleModal();
-            this.props.addComment(this.props.dishId,values.rating,values.author,values.comment);
+            this.props.postComment(this.props.dishId,values.rating,values.author,values.comment);
         }
     render() {
         return (
@@ -101,7 +101,7 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({comments,addComment,dishId}){
+function RenderComments({comments,postComment,dishId}){
 
     return (                
     <div  className="col-12 col-md-5 m-1">
@@ -116,7 +116,7 @@ function RenderComments({comments,addComment,dishId}){
                     </div>
                 })}
             </CardText>
-            <CommentForm dishId={dishId} addComment={addComment}/>
+            <CommentForm dishId={dishId} postComment={postComment}/>
         </CardBody>
       </Card>
     </div>)
@@ -167,7 +167,7 @@ function RenderDish({dish}){
             </div>
                     <div className="row">
                          <RenderDish dish={props.dish}/>
-                         <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id}/>
+                         <RenderComments comments={props.comments} postComment={props.postComment} dishId={props.dish.id}/>
                     </div>
                 </div>)
             }
--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -8,7 +8,7 @@ import Contact from './ContactComponent'
 import {Switch,Route,Redirect,withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
 import About from './AboutComponent'
-import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 
@@ -21,7 +21,7 @@ const mapStateToProps = state => {
   }
 }
 const mapDispatchToProps = dispatch => ({
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
   fetchDishes: () => { dispatch(fetchDishes())},
   resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
   fetchComments: () => dispatch(fetchComments()),
@@ -37,7 +37,8 @@ class Main extends Component {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
-  }
+    
+  };
   
 
   render() {
@@ -59,7 +60,7 @@ class Main extends Component {
             errMess={this.props.dishes.errMess}
             comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
             commentsErrMess={this.props.comments.errMess}
-            addComment={this.props.addComment}
+            postComment={this.props.postComment}
           />)
     }
     return (
